961983854bb718e7dc410c545283aa44361da080
cuixiaonan2000 <397685054@qq.com>
2020-04-08 22:22:37 +0800

    小张:写了一个sayHi方法

---
+++ src/com/rj/bd/user/action/UserAaction.java
@@ -0,0 +1,12 @@
+package com.rj.bd.user.action;
+/**
+ * @desc   User模块C层
+ * @author 小张
+ * @time   2020-04-08
+ */
+public class UserAaction {
+
+	public void sayHi(){
+		System.out.println("Hello!");
+	}
+}
